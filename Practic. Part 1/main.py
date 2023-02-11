a0d71bbcbb788918103586a4a62b637f668a8be1
stastashpulatov <124768414+stastashpulatov@users.noreply.github.com>
2023-02-11 18:18:38 +0500

    Add files via upload

---
+++ Practic. Part 1/main.py
@@ -0,0 +1,21 @@
+import simple_draw as sd
+
+lenght = 200
+point = sd.get_point(300, 300)
+
+
+def triangle(point, angle=0):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=200, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=200, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=200, width=3)
+    v3.draw()
+
+point_0 = sd.get_point(300, 300)
+for angle in range(0, 361, 30):
+    triangle(point=point_0, angle=angle)
+
+sd.pause()
