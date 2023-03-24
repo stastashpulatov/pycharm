6544e6aac7c9cdd4e3d06e44177dd0baf6054b86
stastashpulatov <124768414+stastashpulatov@users.noreply.github.com>
2023-03-24 14:26:38 +0500

    Add files via upload

---
+++ Overlooking the properties and methods of the parent/main.py
@@ -0,0 +1,61 @@
+# Переопределение атрибутов объетка и класса
+# - используется если у дочерного класса отличные свойства
+
+
+class Cat:
+    has_tail = True
+    woolliness = 20 # https://goo.gl/V2NcBf
+
+    def __init__(self, name='Мурзик'):
+        self.name = name
+
+    def __str__(self):
+        return '{} {} хвост есть?  {}, пушистость -  {}'.format(self.__class__.__name__, self.name,
+                                                                self.has_tail, self.woolliness)
+
+
+class Bobtail(Cat):
+    has_tail = False
+
+
+class Sphinx(Cat):
+    woolliness = 1
+
+
+murzik = Bobtail(name='Мурзик')
+sonya = Sphinx(name='Соня')
+print(murzik)
+print(sonya)
+
+# Переопределение методов
+# - используется если у порождённого класса должно отличаться поведение
+class Robot:
+
+    def __init__(self, model):
+        self.model = model
+
+    def __str__(self):
+        return '{} model {}'.format(self.__class__.__name__, self.model)
+
+    def operate(self):
+        print('Робот ездит по кругу')
+
+
+class WarRobot(Robot):
+
+    def operate(self):
+        print('Робот охраняет военный объект')
+
+
+class VacuumCleaningRobot(Robot):
+    def operate(self):
+        print('Робот пылесосит пол')
+
+
+r2d2 = WarRobot(model='R2D2')
+print(r2d2)
+r2d2.operate()
+
+roomba = VacuumCleaningRobot(model='roomba M505')
+print(roomba)
+roomba.operate()
---
+++ Overlooking the properties and methods of the parent/probe.py
@@ -0,0 +1,30 @@
+class Robot:
+
+    def __init__(self, model):
+        self.model = model
+
+    def __str__(self):
+        return '{} model {}'.format(self.__class__.__name__, self.model)
+
+    def operate(self):
+        print('Робот ездит по кругу')
+
+
+class WarRobot(Robot):
+
+    def operate(self):
+        print('Робот охраняет военный объект')
+
+
+class VacuumCleaningRobot(Robot):
+    def operate(self):
+        print('Робот пылесосит пол')
+
+
+r2d2 = WarRobot(model='R2D2')
+print(r2d2)
+r2d2.operate()
+
+roomba = VacuumCleaningRobot(model='roomba M505')
+print(roomba)
+roomba.operate()
