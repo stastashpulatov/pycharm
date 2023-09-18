cb4721193b2c9a89f15246c98bddc9dfdae042a0
stastashpulatov <124768414+stastashpulatov@users.noreply.github.com>
2023-09-18 13:28:33 +0300

    Add files via upload

---
+++ get_mean_size.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def get_mean_size(ls_output: str) -> float:
+    summ_file = 0
+    count = 0
+    for line in ls_output:
+        count += 1
+        summ_file += int(line.split()[4])
+    return summ_file / count
+
+
+if __name__ == '__main__':
+    data: str = sys.stdin.read()[1:]
+    if not data:
+        print('Данная директория пуста или к ней нет доступа.')
+    else:
+        mean_size: float = get_mean_size(data)
+        print(f'Средний размер файлов в данной дериктории: {mean_size} байт')
+
