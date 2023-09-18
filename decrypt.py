0f20967aa8f2ccb96cfda01561f61f6ae79a77d9
stastashpulatov <124768414+stastashpulatov@users.noreply.github.com>
2023-09-18 13:50:04 +0300

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,23 @@
+import sys
+import re
+
+
+def decrypt(encryption: str) -> str:
+    result: list = []
+    dots: int = 0
+    for symbol in encryption:
+        if symbol != '.':
+            result.append(symbol)
+            dots = 0
+            continue
+
+        dots += 1
+        if dots == 2 and result:
+            result.pop()
+            dots = 0
+    return''.join(result)
+
+if __name__ == '__main__':
+    data: str = sys.stdin.read()
+    decryption: str = decrypt(data)
+    print(decryption)
